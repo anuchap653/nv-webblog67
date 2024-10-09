d29904c3c4b8d74df1d75eeeed543a7ca1d60da8
AITANUCHA <anuchap65@nu.ac.th>
2024-10-09 20:57:20 +0700

    แก้ client  blog

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -2,19 +2,32 @@
   <div>
     <div class="nv-navbar">
       <ul class="nav">
+<<<<<<< HEAD
         <li><router-link :to="{ name: 'blogs' }">Blogs</router-link></li>
         <li><router-link :to="{ name: 'users' }">Users</router-link></li>
         <li><router-link :to="{ name: 'comments' }">Comments</router-link></li>
         <!-- if isLogin is true, show logout menu. otherwise, show login menu-->
         <li><router-link :to="{ name: 'login' }">Login</router-link></li>  
+=======
+        <li><router-link :to="{ name: 'blogs' }">Product</router-link></li>
+        <li><router-link :to="{ name: 'users' }">Users</router-link></li>
+        <!-- Login menu will be aligned to the right -->
+        <li class="right"><router-link :to="{ name: 'login' }">Login</router-link></li>  
+>>>>>>> a03cca9 (แก้blog client ให้แสดงรูปได่)
       </ul>
     </div>
   </div>
 </template>
+<<<<<<< HEAD
 <script>
 
 export default {
 
+=======
+
+<script>
+export default {
+>>>>>>> a03cca9 (แก้blog client ให้แสดงรูปได่)
   methods:{
     isLogin(){
       return this.$store.getters.isUserLoggedIn;
@@ -26,6 +39,10 @@ export default {
   }
 };
 </script>
+<<<<<<< HEAD
+=======
+
+>>>>>>> a03cca9 (แก้blog client ให้แสดงรูปได่)
 <style scoped>
 .nv-navbar {
   background-color: palegoldenrod;
@@ -38,10 +55,20 @@ export default {
   margin: 0;
   padding: 0;
   float: left;
+<<<<<<< HEAD
+=======
+  width: 100%; /* To allow float right to work properly */
+>>>>>>> a03cca9 (แก้blog client ให้แสดงรูปได่)
 }
 .nv-navbar .nav li {
   float: left;
 }
+<<<<<<< HEAD
+=======
+.nv-navbar .nav li.right {
+  float: right; /* Align the Login menu to the right */
+}
+>>>>>>> a03cca9 (แก้blog client ให้แสดงรูปได่)
 .nv-navbar .nav li a {
   padding: 10px;
   text-decoration: none;
