29b56adccdada7a6300dbef38c543c10bd85bad9
AITANUCHA <anuchap65@nu.ac.th>
2024-10-09 20:53:52 +0700

    แก้ client  blog

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -2,17 +2,19 @@
   <div>
     <div class="nv-navbar">
       <ul class="nav">
-        <li><router-link :to="{ name: 'blogs' }">Product</router-link></li>
+        <li><router-link :to="{ name: 'blogs' }">Blogs</router-link></li>
         <li><router-link :to="{ name: 'users' }">Users</router-link></li>
-        <!-- Login menu will be aligned to the right -->
-        <li class="right"><router-link :to="{ name: 'login' }">Login</router-link></li>  
+        <li><router-link :to="{ name: 'comments' }">Comments</router-link></li>
+        <!-- if isLogin is true, show logout menu. otherwise, show login menu-->
+        <li><router-link :to="{ name: 'login' }">Login</router-link></li>  
       </ul>
     </div>
   </div>
 </template>
-
 <script>
+
 export default {
+
   methods:{
     isLogin(){
       return this.$store.getters.isUserLoggedIn;
@@ -24,7 +26,6 @@ export default {
   }
 };
 </script>
-
 <style scoped>
 .nv-navbar {
   background-color: palegoldenrod;
@@ -37,13 +38,9 @@ export default {
   margin: 0;
   padding: 0;
   float: left;
-  width: 100%; /* To allow float right to work properly */
 }
 .nv-navbar .nav li {
   float: left;
-}
-.nv-navbar .nav li.right {
-  float: right; /* Align the Login menu to the right */
 }
 .nv-navbar .nav li a {
   padding: 10px;
